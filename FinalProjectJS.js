765ba0b871abcf2813089523b2cf56e250ae0606
Baina16 <45863727+Baina16@users.noreply.github.com>
2018-12-14 19:15:16 -0600

    Add files via upload

---
+++ FinalProjectJS.js
@@ -0,0 +1,9 @@
+var todaysDate = new Date();
+		todaysDate.setDate();
+		document.getElementById("todaysDate").innerHTML = todaysDate;
+
+		function todaysDate() {
+
+			document.getElementById("todaysDate").readonly = true;
+			
+		}
